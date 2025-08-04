138fbbe155f2a67353ec3c1d750de0d5030cf8e6
Baldwine-86 <159168161+Baldwine-86@users.noreply.github.com>
2025-08-04 23:17:17 +0300

    updated

---
+++ answer.sql
@@ -0,0 +1,7 @@
+-- create a new database called SalesDB
+-- CREATE DATABASE is used to create a new database.
+CREATE DATABASE SalesDB;
+
+-- drop database called demo.
+-- DROP DATABASE is used to delete existing databases.
+DROP DATABASE demo;
